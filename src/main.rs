43565016a0d8fe76f46d88af372ed4ff7c7d33dc
Taufiq J <topekjm@gmail.com>
2024-02-24 23:38:57 +0700

    Add sha256 library & update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::{cmp::Ordering, ops::Index};
+use sha256::try_digest;
+
+fn get_sha(filename: String) -> String{
+    return try_digest(filename).unwrap();
+}
+
+fn compare(sha1: String, sha2: String) -> bool {
+    return sha1.cmp(&sha2) == Ordering::Equal;
+}
+
+fn show_sha(filename: String) {
+    println!("{}", get_sha(filename));
+}
+
+fn show_compare(filename: String, given_sha: String) {
+    println!("{}", compare(get_sha(filename), given_sha));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+
+    match args.len() {
+        1 => eprintln!("You need to insert file name as an argument"),
+        2 => show_sha(args.index(1).to_string()),
+        3 => show_compare(args.index(1).to_string(), args.index(2).to_string()),
+        _ => panic!("Panic when checking args length")
+    }
+    // if args.len() == 1 {
+    //     return;
+    // } else if args.len() == 2 {
+
+    // }
+
+    // let filename = args.index(1);
+    
+    // println!("{} : {}", filename, );
 }
